b03d59212cf09f90a57d38b9fefa6e3eb3f4b9e1
Lisal Orion Djara <2481021@unai.edu>
2026-03-12 22:15:27 +0700

    first commit

---
+++ src/main/java/com/lisaloriondjara/Handson6/Category.java
@@ -0,0 +1,12 @@
+package com.lisaloriondjara.Handson6;
+
+public enum Category {
+    ELEKTRONIK,
+    PAKAIAN,
+    MAKANAN,
+    MINUMAN,
+    PERALATAN_RUMAH,
+    OLAHRAGA,
+    KESEHATAN,
+    LAINNYA
+}
